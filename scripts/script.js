3de928c0184d251ab3f2d164049d756c54d6b9f4
Lom070707 <maksimlomanuk77@gmail.com>
2022-01-09 13:12:59 +0200

    added madal window

--- scripts/script.js
+++ scripts/script.js
@@ -16,3 +16,49 @@ document.addEventListener('click', (event) => {
     }
 
 })
+
+
+
+
+//Calculator
+const calculator = document.getElementById('calculator');
+function calculate() {
+    let data = prompt('Привет, я калькулятор! Что желаете посчтитать?')
+    if (data === null) {
+        alert('Ввод отменен')
+    } else if (data === '') {
+        alert('Вы ничего не ввели!')
+    } else {
+        data = data.replace(/,/g, '.')
+        try {
+            let result = eval(data)
+            if (result === Infinity || result === -Infinity) {
+                alert('На ноль делить нельзя!')
+            } else if (isNaN(result)) {
+                alert('Введите корректное выражение')
+            } else {
+                alert(result)
+            }
+        } catch {
+            alert('Введите корректное выражение')
+        }
+
+    }
+
+}
+calculator.addEventListener('click', calculate)
+
+
+//modal-window
+const order = document.getElementById('order')
+const modal = document.getElementById('modal')
+const closeWindow = document.getElementById('close-window')
+order.addEventListener('click', function (event) {
+    event.preventDefault();//отменяем действия браузера по умолчанию
+    modal.style.display = 'flex'
+    document.documentElement.style.overflow = 'hidden'
+})
+closeWindow.addEventListener('click', function (event) {
+    modal.style.display = 'none'
+    document.documentElement.style.overflow = 'auto'
+})
